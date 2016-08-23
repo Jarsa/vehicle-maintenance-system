207c94ce77955e00a7d53eeebc8e40070608f07b
ougc27 <oscar_garzacordova@hotmail.com>
2016-08-23 11:58:07 -0500

    [ADD][models]- hr_employee, vms_ativity_time, vms_order, vms_product_line, vms_report, vms_vehicle_cycle

---
+++ models/vms_order.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# © <2016> <Jarsa Sistemas, S.A. de C.V.>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from openerp import fields, models
+
+
+class VmsOrder(models.Model):
+    _description = 'VMS Orders'
+    _name = 'vms.order'
+
+    name = fields.Char(string='Order Number', required=True, readonly=True)
+    base_id = fields.Char(
+        required=True,
+        string='Base')
+    supervisor_id = fields.Many2one(
+        'hr.employee',
+        required=True,
+        domain=[('mechanic', '=', True)],
+        string='Supervisor')
+    date = fields.Datetime(
+        required=True,
+        default=fields.Datetime.now)
+    current_odometer = fields.Float(default=0.0)
+    type = fields.Selection(
+        [('preventive', 'Preventive'),
+         ('corrective', 'Corrective')],
+        required=True)
+    stock_location_id = fields.Many2one(
+        'stock.location',
+        required=True,
+        string='Stock Location')
+    start_date = fields.Datetime(
+        required=True,
+        default=fields.Datetime.now,
+        string='Schedule start')
+    end_date = fields.Datetime(
+        required=True,
+        # compute= '_compute_end_date'
+        string='Schedule end'
+        )
+    start_date_real = fields.Datetime(
+        required=True,
+        readonly=True,
+        string='Real start date')
+    end_date_real = fields.Datetime(
+        required=True,
+        readonly=True,
+        string='Real end date'
+        )
+    order_line_ids = fields.One2many(
+        'vms_order_line',
+        'order_id',
+        string='Activities')
+    program_id = fields.Many2one(
+        'vms.program',
+        string='Program')
+    cycle_id = fields.Many2one(
+        'vms.cycle',
+        string='Cycle')
+    sequence = fields.Integer()
+    report_ids = fields.Many2Many(
+        'vms.report',
+        domain=[('state', '=', 'confirmed')],
+        string='Report(s)')
+    state = fields.Selection(
+        [('draft', 'Draft'),
+         ('open', 'Open'),
+         ('released', 'Released')],
+        string='State',
+        readonly=True)
