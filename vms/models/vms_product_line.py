eadafe99b6d84c7ce16ba86fc7e664d42ff5f161
Hector-Camacho <hector.camacho.f@gmail.com>
2016-08-30 10:59:12 -0500

    [ADD][vms.product.line] Added compute_state function and action_cancel function for product line [REF][vms.product.line] Replaced stock_move_id by stock_picking_id because stock_picking needs to be de parent of all the moves of the spares

--- vms/models/vms_product_line.py
+++ vms/models/vms_product_line.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from datetime import datetime
-from openerp import api, fields, models
+from openerp import _, api, exceptions, fields, models
 
 
 class VmsProductLine(models.Model):
@@ -32,16 +32,16 @@ class VmsProductLine(models.Model):
     state = fields.Selection(
         [('draft', 'Draft'),
          ('open', 'Open'),
+         ('cancel', 'Cancelled'),
          ('released', 'Released')],
         readonly=True, default='draft')
-    stock_move_id = fields.Many2one(
-        'stock.move',
-        string='Stock Move',
+    stock_picking_id = fields.Many2one(
+        'stock.picking',
+        string='Stock Picking',
         readonly=True)
 
     @api.multi
     def create_stock_picking(self, location_id, product_id, product_uom_id):
-        stock_picking = self.env['stock.move']
         for rec in self:
             today = datetime.today.now()
             seller_id = [x[0] for x in rec.product_id.seller_ids]
@@ -63,7 +63,27 @@ class VmsProductLine(models.Model):
                 'move_lines': [move],
                 'type': 'in',
             }
-            stock_picking.create(picking)
+            rec.stock_picking_id.create(picking)
+
+    @api.multi
+    def compute_state(self):
+        for rec in self:
+            if rec.stock_picking_id.state == 'done':
+                rec.state = 'released'
+            elif rec.stock_picking_id == 'cancel':
+                rec.state = 'cancel'
+
+    @api.multi
+    def action_cancel(self):
+        for rec in self:
+            for stock in rec.stock_picking_id:
+                if stock.state == 'done':
+                    raise exceptions.ValidationError(
+                        _('Warning! The stock'
+                          'picking is already delivered'))
+                else:
+                    stock.state = 'cancel'
+            rec.state = 'cancel'
 
     @api.onchange('product_id')
     def _onchange_product_id(self):
