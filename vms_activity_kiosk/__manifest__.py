14cdca92a5b056c86e255e8b9e1c05e6a8b5f005
AlexisReza <alexis.reza.s@gmail.com>
2018-01-31 17:48:48 -0600

    [ADD][vms_tasks_kiosk] Added Kiosk Mode for mechanics tasks

---
+++ vms_activity_kiosk/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Tasks Kiosk Mode",
+    "summary": "Tasks Kiosk Mode",
+    "version": "10.0.1.0.0",
+    "category": "Maintenance",
+    "author": "Jarsa Sistemas",
+    "website": "https://www.jarsa.com.mx",
+    "depends": [
+        'vms_activity'
+    ],
+    "license": "AGPL-3",
+    "data": [
+        "views/assets.xml",
+        "views/hr_employee_view.xml",
+        "views/tasks_kiosk_view.xml",
+    ],
+    'qweb': [
+        "static/src/xml/vms_kiosk_templates.xml",
+    ],
+    "installable": True,
+}
