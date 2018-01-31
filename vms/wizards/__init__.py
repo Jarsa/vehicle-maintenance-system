ce1e49bc13fe8c2e1596beba9054ab5ce5cb4769
lmgr0312 <lmiguel.gruiz@gmail.com>
2018-01-31 10:59:24 -0600

    [WIP][FIX CONFLICTS]

--- vms/wizards/__init__.py
+++ vms/wizards/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016, Jarsa Sistemas, S.A. de C.V.
+# Copyright 2018, Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 from . import vms_wizard_maintenance_order
+from . import vms_order_line_po
