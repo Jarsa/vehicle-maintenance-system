0867952805cd13c7b43897da29872370d5eadff4
SaraiOsorio <sarai.osorio@jarsa.com.mx>
2018-02-15 18:39:52 -0600

    [ADD][vms]test vms_product_line

---
+++ vms/tests/test_vms_product_line.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo.exceptions import ValidationError
+from odoo.tests.common import TransactionCase
+
+
+class TestVmsProductLine(TransactionCase):
+
+    def setUp(self):
+        super(TestVmsProductLine, self).setUp()
+        self.product = self.env.ref('vms.product_product_vms_02')
+        self.vms_prod_line = self.env.ref('vms.vms_product_line_01')
+
+    def test_10_onchange_product_id(self):
+        self.vms_prod_line.product_id = self.product.id
+        self.vms_prod_line._onchange_product_id()
+        self.assertEqual(
+            self.vms_prod_line.product_uom_id, self.product.uom_id)
